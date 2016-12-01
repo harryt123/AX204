0a6f717e4bb3a0510c1cdc2d3404a2738a59ea79
harryt123 <haroldtsui@gmail.com>
2016-12-01 16:50:47 +0800

    lol

--- Lesson5/app.js
+++ Lesson5/app.js
@@ -61,10 +61,10 @@ function update() {
 	player.body.velocity.x = 0;
 	if (cursors.left.isDown) {
 		player.body.velocity.x= -150;
-		player.animations.play("left");
+		player.animations.play("right");
 	}else if (cursors.right.isDown) {
 		player.body.velocity.x= 150;
-		player.animations.play("right");
+		player.animations.play("left");
 	}else {
 		player.animations.stop();
 		player.frame = 4;
