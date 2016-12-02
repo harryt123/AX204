4ed3ae98ff5cecbe9d154b5ef62c82595bde9126
harryt123 <haroldtsui@gmail.com>
2016-12-02 10:53:21 +0800

    PHASER

--- Lesson5/app.js
+++ Lesson5/app.js
@@ -46,7 +46,7 @@ function create() {
 		player.animations.add('right', [6, 7, 8, 9], 10, true);
 		//physics
 		game.physics.arcade.enable(player);
-		player.body.bounce.y = 0.2;
+		player.body.bounce.y = 0.21;
 		player.body.gravity.y = 300;
 		player.body.collideWorldBounds = true;
 
