4c30f05a30c5b9464382571e72d874b495054ee2
harryt123 <haroldtsui@gmail.com>
2016-12-08 16:54:37 +0800

    PHASER

--- Lesson5/app.js
+++ Lesson5/app.js
@@ -69,6 +69,7 @@ function update() {
 	game.physics.arcade.collide(enemy, platforms);
 	game.physics.arcade.collide(stars, platforms);
 	game.physics.arcade.collide(stars, player);
+	game.physics.arcade.collide(enemy, player);
 	player.body.velocity.x = 0;
 	if (cursors.left.isDown) {
 		player.body.velocity.x= -150;
